a1a5b9db158bcd0bd8f5f9baa3d8082a71f1782f
asmitt8373 <austinsmittle8373@gmail.com>
2022-08-16 22:21:09 -0700

    updated readme

--- Develop/readMe.md
+++ Develop/readMe.md
@@ -1,7 +1,6 @@
-# jdlkfja
- undefined
+# README Generator
  ## Description
- #dlfjadlkfj
+ #This project asks users to enter information to make them a read me file. This application creates a Table of Contents, and also a github badge for the User.
  ## Table of Contents
  * [Installation](#installation)
  * [Usage](#usage)
@@ -10,17 +9,19 @@
  * [Tests](#tests)
  * [Questions](#questions)
  ## Installation
- dllfkjdslkajf
+ use npm i and inquierer
  ## Usage
- aldfjakld
+ To create a read me file with prompts on the command line.To start go to the command line and type "node index.js"
+ ## License
  undefined
+ ![GitHub badge](https://img.shields.io/badge/License-GPLv3-blue.svg)
  ## Contributing
  
- flkajdflk
+ github
  
  ## Tests
- daljfkdjf
+ To test to make sure you have everything you need put "npm run test" in your command line.
  ## Questions
- My GitHub: [adklfjdkljf](https://github.com/adklfjdkljf)
- Email me: dlfjkdfal
+ My GitHub: [asmitt8373](https://github.com/asmitt8373)
+ Email me: austinsmittle8373@gmail.com
 }
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,42 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  var listBadge;
-        if (license === 'Apache License 2.0') {
-          listBadge = 'https://img.shields.io/badge/Apache%20License%202.0-license-black'
-        } else if (license === 'GNU General Public License v3.0'){
-          listBadge = 'https://img.shields.io/badge/GNU%20General%20Public%20License%20v3.0-license-black'
-        } else if (license === 'BSD 3-Clause "New" or "Revised" License') {
-          listBadge = 'https://img.shields.io/license/BSD%203--Clause%60-badge-black'
-        } else if (license === 'MIT License') {
-          listBadge = 'https://img.shields.io/license/MIT-badge-black'
-        };
-  }
+function renderLicenseBadge(license) {} 
 
 
 // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-  
-// }
+function renderLicenseLink(license) {}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-//   if (license.length === 0) {
-//     return ""
-//   } else {
-//     return `## License
-//   ${renderLicenseBadge(license)}
-  
-//   ${renderLicenseLink(license)} 
-//     `
-//   }
-// }
+function renderLicenseSection(license) {
+  if (license === 'Apache License 2.0') {
+    listBadge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
+  } else if (license === 'GNU General Public License v3.0'){
+    listBadge = 'https://img.shields.io/badge/License-GPLv3-blue.svg'
+  } else if (license === 'BSD 3-Clause "New" or "Revised" License') {
+    listBadge = 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg'
+  } else if (license === 'MIT License') {
+    listBadge = '(https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  };
+}
+ 
+ 
+
 
 //this makes a skeleton of what the readme will look like and the "data." will add certain data to that area
 function generateMarkdown(data) {
   return `# ${data.title}
- ${renderLicenseBadge(data.license)}
  ## Description
  #${data.description}
  ## Table of Contents
@@ -50,7 +40,9 @@ function generateMarkdown(data) {
  ${data.installation}
  ## Usage
  ${data.usage}
+ ## License
  ${renderLicenseSection(data.license)}
+ ![GitHub badge](${listBadge})
  ## Contributing
  
  ${data.contribution}
